fix sum normalization bug

DeepLP.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 
 class DeepLP:
@@ -34,16 +35,9 @@ class DeepLP:
     def get_val(self,val):
         return self.sess.run(val)
 
-
-    i = tf.constant(0)
-    while_condition = lambda i: tf.less(i, input_placeholder[1, 1])
-
-
-    # do the loop:
-
     def forwardprop(self):
-        T = self.W / tf.reduce_sum(self.W, axis = 0)
-        Tnorm = tf.transpose(T / tf.reduce_sum(T, axis = 1))
+        T = self.W / tf.reduce_sum(self.W, axis = 0, keep_dims=True)
+        Tnorm = T / tf.reduce_sum(T, axis = 1, keep_dims=True)
 
         trueX = self.X
         X = self.X
@@ -54,15 +48,9 @@ class DeepLP:
             return [i+1,h,trueX,Tnorm]
 
         def condition(i,X,trueX,Tnorm):
-            return self.iter_ >= i
+            return self.iter_ > i
 
         _,h,_,_ = tf.while_loop(condition, layer, loop_vars=[0,X,trueX,Tnorm])
-
-
-        # for i in range(self.iter_):
-        #     h = X @ Tnorm
-        #     h = tf.multiply(h, self.unlabeled) + tf.multiply(trueX, self.labeled)
-        #     X = h
         return h
 
     def backwardprop(self):
@@ -145,3 +133,27 @@ class DeepLP:
                 self.eval(self.updates,data)
             self.save(epoch,data,full_data,n)
         # self.close_sess()
+
+    def plot_loss(self,):
+        plt.plot(self.labeled_losses,label="labeled loss")
+        plt.plot(self.unlabeled_losses,label="unlabeled loss")
+        plt.plot(self.sol_unlabeled_losses,label='validation unlabeled loss')
+        plt.title("loss")
+        plt.legend()
+        plt.show()
+
+    def plot_accuracy(self):
+        plt.plot(self.accuracies,label="DeepLP, train")
+        plt.plot([self.sol_accuracies[0]] * len(self.accuracies),label="LP")
+        plt.plot(self.sol_accuracies,label="DeepLP, validation")
+        plt.title("accuracy")
+        plt.legend()
+        plt.show()
+
+    def plot_params(self):
+        pass
+
+    def plot(self):
+        self.plot_loss()
+        self.plot_accuracy()
+        self.plot_params()

DeepLP_Sparse.py
@@ -1,136 +1,20 @@
 from __future__ import print_function
 import tensorflow as tf
 import numpy as np
+import time
+import matplotlib.pyplot as plt
+
 
 class DeepLP_Sparse:
-    def __init__(self, iter_, num_nodes, weights, lr, regularize=0):
-        self.W      = self.init_weights(weights)
-        self.regularize = regularize
-        self.build_graph(iter_,lr,num_nodes)
 
-
-    def build_graph(self,iter_,lr,num_nodes):
-        self.lr     = lr
-        self.iter_  = iter_ # Layer size
-
-        shape             = [None, num_nodes]
-        self.X            = tf.sparse_placeholder("float", shape=shape)
-        self.y            = tf.sparse_placeholder("float", shape=shape)
-        self.unlabeled    = tf.sparse_placeholder("float", shape=shape)
-        self.labeled      = tf.sparse_placeholder("float", shape=shape)
-        self.masked       = tf.sparse_placeholder("float", shape=shape)
-        self.true_labeled = tf.sparse_placeholder("float", shape=shape)
-
-        self.yhat = self.forwardprop()
-        self.backwardprop()
+    def dense_to_sparse(self,a_t):
+        idx = tf.where(tf.not_equal(a_t, 0))
+        # Use tf.shape(a_t, out_type=tf.int64) instead of a_t.get_shape() if tensor shape is dynamic
+        sparse = tf.SparseTensor(idx, tf.gather_nd(a_t, idx), a_t.get_shape())
+        return sparse
 
     def init_weights(self,weights_np):
         """ Weight initialization """
-        weights = self.convert_to_sparse_tensor(weights_np)
-        return tf.Variable(weights)
-
-    def convert_to_sparse_tensor(self,np_mat):
-        weights = tf.convert_to_tensor(np_mat, np.float32)
-        idx = tf.where(tf.not_equal(weights, 0))
-        sparse = tf.SparseTensor(idx, tf.gather_nd(weights, idx), weights.get_shape())
-        return sparse
-
-    def get_value(self,val):
-        return self.sess.run(val)
-
-    def sparse_transpose(self,mat):
-        input_st = mat
-        indices = input_st.indices
-        # Reverse the columns of `indices`
-        transposed_indices = tf.concat(1, [indices[:, 1:2], indices[:, 0:1]])
-
-        dense_shape = input_st.dense_shape
-        # Reverse the elements of `dense_shape`
-        transposed_dense_shape = tf.pack([dense_shape[1], dense_shape[0]])
-
-        transposed_st = tf.SparseTensor(transposed_indices, input_st.values, transposed_dense_shape)
-        return transposed_st
-
-    def forwardprop(self):
-        T = self.W / tf.sparse_reduce_sum(self.W, axis = 0)
-
-        Tnorm = self.sparse_transpose(T / tf.sparse_reduce_sum(T, axis = 1))
-
-        trueX = self.X
-        X = self.X
-
-        for i in range(self.iter_):
-            h = tf.sparse_matmul(X, Tnorm)
-            h = h.__mul__(self.unlabeled) + trueX.__mul__(self.labeled)
-            X = h
-        return h
-
-    def backwardprop(self):
-        # Backward propagation
-        if self.regularize:
-            self.loss = self.calc_loss(self.masked,self.y,self.yhat) + self.regularize*tf.nn.l2_loss(self.theta-1)
-        else:
-            self.loss = self.calc_loss(self.masked,self.y,self.yhat)
-        self.updates = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
-
-    def calc_loss(self,mask,y,yhat):
-        loss_mat = tf.multiply(mask, (y-yhat) ** 2 )
-        return tf.reduce_sum(loss_mat) / tf.count_nonzero(loss_mat,dtype=tf.float32)
-
-    def calc_accuracy(self,y,yhat,full=False):
-        if full:
-            acc_mat = tf.multiply((1-self.true_labeled),tf.cast(tf.equal(tf.round(yhat),y),tf.float32))
-            return tf.reduce_sum(acc_mat) / tf.count_nonzero((1-self.true_labeled),dtype=tf.float32)
-        else:
-            return tf.reduce_mean(tf.cast(tf.equal(tf.round(yhat),y),tf.float32))
-
-    def labelprop(self,data):
-        init = tf.global_variables_initializer()
-        self.sess.run(init)
-
-        pred = self.eval(self.yhat,data)
-        return pred
-
-    def eval(self,tensor,data):
-        return self.sess.run(tensor, feed_dict={self.X:data['X'],
-                                    self.y:data['y'],
-                                    self.unlabeled:data['unlabeled'],
-                                    self.labeled:data['labeled'],
-                                    self.masked:data['masked'],
-                                    self.true_labeled:data['true_labeled']})
-
-    def save_params(self,epoch,data,n):
-        pass
-
-    def save(self,epoch,data,full_data,n):
-        labeled_loss = self.eval(self.loss,data)
-        unlabeled_loss = self.eval(self.calc_loss((1-self.true_labeled),self.y,self.yhat),data)
-        accuracy = self.eval(self.calc_accuracy(self.y,self.yhat),data)
-        sol_unlabeled_loss = self.eval(self.calc_loss((1-self.true_labeled),self.y,self.yhat),full_data)
-        sol_accuracy = self.eval(self.calc_accuracy(self.y,self.yhat,True),full_data)
-        acc_mat = tf.multiply((1-self.true_labeled),tf.cast(tf.equal(tf.round(self.yhat),self.y),tf.float32))
-        self.labeled_losses.append(labeled_loss)
-        self.unlabeled_losses.append(unlabeled_loss)
-        self.accuracies.append(accuracy)
-        self.sol_accuracies.append(sol_accuracy)
-        self.sol_unlabeled_losses.append(sol_unlabeled_loss)
-        if epoch % 10 == 0 or epoch == -1:
-            print("epoch:",epoch,"labeled loss:",labeled_loss,"unlabeled loss:",unlabeled_loss,"accuracy:",accuracy,"sol unlabeled loss:",sol_unlabeled_loss,"sol accuracy:",sol_accuracy)
-        self.save_params(epoch,data,n)
-
-    def train(self,data,full_data,epochs):
-        init = tf.global_variables_initializer()
-        self.sess.run(init)
-        n = len(data['X'])
-        self.labeled_losses = []
-        self.unlabeled_losses = []
-        self.accuracies = []
-        self.sol_accuracies = []
-        self.sol_unlabeled_losses = []
-        self.save(-1,data,full_data,n)
-
-        for epoch in range(epochs):
-            # Train with each example
-            for i in range(n):
-                self.eval(self.updates,data)
-            self.save(epoch,data,full_data,n)
+        weights = tf.convert_to_tensor(weights_np, np.float32)
+        sparse_weights = self.dense_to_sparse(weights)
+        return tf.Variable(sparse_weights)

LP.py
@@ -4,23 +4,24 @@ from numpy.linalg import inv
 
 
 class LP:
+    def __init__(self,num_nodes):
+        self.num_nodes = num_nodes
 
-    def tnorm(self,X):
-        W = rbf_kernel(X)
-        T = W / np.sum(W, axis=0)
-        Tnorm = T / np.sum(T, axis=1)
+    def tnorm(self,W):
+        T = W / np.sum(W, axis=0, keepdims=True)
+        Tnorm = T / np.sum(T, axis=1, keepdims=True)
         return Tnorm
 
-    def closed(self,X,Ly):
+    def closed(self,W,Ly):
         n = len(Ly)
-        Tnorm = self.tnorm(X)
+        Tnorm = self.tnorm(W)
         Tuu_norm = Tnorm[n:,n:]
         Tul_norm = Tnorm[n:,:n]
         Uy_lp = inv((np.identity(len(Tuu_norm))-Tuu_norm)) @ Tul_norm @ Ly
         return Uy_lp
 
-    def iter_(self,X,Ly,Uy,iter_):
-        Tnorm = self.tnorm(X).T
+    def iter_(self,W,Ly,Uy,iter_):
+        Tnorm = self.tnorm(W)
         Y = np.hstack((Ly,Uy))
 
         for i in range(iter_):

utils.py
@@ -3,16 +3,7 @@ import numpy as np
 import scipy as sp
 from sklearn import datasets
 
-def get_iris_data(label_prob=0.05):
-    """ Read the iris data and label/unlabel data points"""
-    # load iris data
-    iris   = datasets.load_iris()
-    data   = iris["data"]
-    labels = iris["target"]
-
-    # get label 0 and 1, and corresponding data
-    labels = labels[labels < 2]
-    data = data[np.where(labels < 2)]
+def random_unlabel(data,labels,label_prob=0.1,hard=False):
 
     # generate random numbers for unlabeling
     rng = np.random.RandomState(42)
@@ -23,7 +14,10 @@ def get_iris_data(label_prob=0.05):
     Uy_sol = np.copy(labels[random_unlabeled_points])
 
     # unlabel points (opposite of the true label, to make the problem hard)
-    masked_labels[random_unlabeled_points] = 1-labels[random_unlabeled_points]
+    if hard:
+        masked_labels[random_unlabeled_points] = 1-masked_labels[random_unlabeled_points]
+    else:
+        masked_labels[random_unlabeled_points] = 0.5
 
     unlabeled_indices = np.where(random_unlabeled_points)
 
@@ -37,24 +31,88 @@ def get_iris_data(label_prob=0.05):
 
     return LX, Ly, UX, Uy, Uy_sol
 
-def rbf_kernel(X,s=None):
+
+def rbf_kernel(X,s=None,G=None,percentile=3):
     # use rbf kernel to estimate weights
     pairwise_dists = squareform(pdist(X, 'euclidean'))
     if not s:
-        s = np.var(X)
+        s = 1
     K = sp.exp(-pairwise_dists ** 2 / s ** 2)
-    threshold = np.percentile(K,10)
-    np.fill_diagonal(K, 0)
+    if G == None:
+        threshold = np.percentile(K,percentile)
+        np.fill_diagonal(K, 0)
 
-    def get_neighbors(arr):
-        # index = arr.argsort()[:-5][::-1]
-        index = np.where(arr < threshold)
-        arr[index] = 0
-        return arr
+        def get_neighbors(arr):
+            # index = arr.argsort()[:-5][::-1]
+            index = np.where(arr < threshold)
+            arr[index] = 0
+            return arr
 
-    K = np.apply_along_axis(get_neighbors, 0, K)
+        K = np.apply_along_axis(get_neighbors, 0, K)
+    else:
+        K = K * G
 
     return K
+
+def parallel_coordinates(frame, class_column, cols=None, ax=None, color=None,
+                     use_columns=False, xticks=None, colormap=None,
+                     **kwds):
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+
+    n = len(frame)
+    class_col = frame[class_column]
+    class_min = np.amin(class_col)
+    class_max = np.amax(class_col)
+
+    if cols is None:
+        df = frame.drop(class_column, axis=1)
+    else:
+        df = frame[cols]
+
+    used_legends = set([])
+
+    ncols = len(df.columns)
+
+    # determine values to use for xticks
+    if use_columns is True:
+        if not np.all(np.isreal(list(df.columns))):
+            raise ValueError('Columns must be numeric to be used as xticks')
+        x = df.columns
+    elif xticks is not None:
+        if not np.all(np.isreal(xticks)):
+            raise ValueError('xticks specified must be numeric')
+        elif len(xticks) != ncols:
+            raise ValueError('Length of xticks must match number of columns')
+        x = xticks
+    else:
+        x = range(ncols)
+
+    fig = plt.figure()
+    ax = plt.gca()
+
+    Colorm = plt.get_cmap(colormap)
+
+    for i in range(n):
+        y = df.iloc[i].values
+        kls = class_col.iat[i]
+        ax.plot(x, y, color=Colorm((kls - class_min)/(class_max-class_min)), **kwds)
+
+    for i in x:
+        ax.axvline(i, linewidth=1, color='black')
+
+    ax.set_xticks(x)
+    ax.set_xticklabels(df.columns)
+    ax.set_xlim(x[0], x[-1])
+    ax.legend(loc='upper right')
+    ax.grid()
+
+    bounds = np.linspace(class_min,class_max,10)
+    cax,_ = mpl.colorbar.make_axes(ax)
+    cb = mpl.colorbar.ColorbarBase(cax, cmap=Colorm, spacing='proportional', ticks=bounds, boundaries=bounds, format='%.2f')
+
+    return fig
+
 
 def accuracy(y,yhat):
     return np.mean((yhat == y))
@@ -63,5 +121,6 @@ def rmse(y,yhat):
     return np.mean((yhat - y)**2)
 
 def objective(Ly,Uy_lp,W):
-    labels = np.hstack((Ly,Uy_lp)).reshape(100,1)
+    n = len(Ly) + len(Uy_lp)
+    labels = np.hstack((Ly,Uy_lp)).reshape(n,1)
     return (labels.T @ W @ labels)[0][0]